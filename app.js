1e1a24fb8b2b7f5d903194434b6b2de63f1bde24
maniksheak <mdmanik081498@gmail.com>
2022-04-06 14:44:20 +0600

    test

---
+++ app.js
@@ -0,0 +1,12 @@
+// Love Calculator
+
+//   Persons Name
+    let firstName = prompt('What is your name?');
+    let partnarName = prompt('What is your partnar name?');
+    
+// Calculation
+   let number = Math.random() * 100;
+      number = Math.floor(number) + 1;
+
+//  Showing Alert
+    alert(firstName + " and " + partnarName + "'s love Parcentage is: " + number + "%");      
